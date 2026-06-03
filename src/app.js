b6365c0068a8da55378ae3c8444930517fb66118
TuvalZit18 <18tuvalzit18@gmail.com>
2026-06-04 01:36:06 +0300

    Finishing exe 1-3 || todo - extention

--- src/app.js
+++ src/app.js
@@ -4,25 +4,28 @@
 */
 
 //Exercise 1
-const findFactorial = function() {
-  //Your code here
-
-}
+const findFactorial = function (number) {
+  if (number === 0 || number === 1) return 1;
+  else return findFactorial(number - 1) * number;
+};
 
 //Exercise 2
-const reverseString = function() {
-  //Your code here
-
-}
+const reverseString = function (str) {
+  if (str.length === 0) return;
+  if (str.length === 1) return str;
+  return str[str.length - 1] + reverseString(str.slice(0, str.length - 1));
+};
 
 //Exercise 3
-const arr1 = [1, 2, 3]
-const arr2 = []
+const arr1 = [1, 2, 3];
+const arr2 = [];
 
-const swap = function() {
-  //Your code here
-
-}
+const swap = function (arr1, arr2) {
+  if (arr1.length === 0) return;
+  arr2.push(arr1[0]);
+  arr1.splice(0, 1);
+  return swap(arr1, arr2);
+};
 
 /* DO NOT REMOVE THE EXPORTS BELOW */
-module.exports = { findFactorial, reverseString, swap }
+module.exports = { findFactorial, reverseString, swap };
